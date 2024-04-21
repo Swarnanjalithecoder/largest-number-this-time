1491f1fe266e6a8cdb3c7aa10c422b1b69f2402c
Swarnanjalithecoder <167692088+Swarnanjalithecoder@users.noreply.github.com>
2024-04-21 23:22:26 +0530

    Create tds_app.py

---
+++ tds_app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+def FindLargest(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+def main():
+    st.title("TDS Graded Assignment Week8 : Find the Largest Among the Three Given Numbers")
+    
+  
+    st.write("Enter three numbers.")
+
+    num1 = st.number_input("Enter the First number:", step=1.0)
+    num2 = st.number_input("Enter the Second number:", step=1.0)
+    num3 = st.number_input("Enter the Third number:", step=1.0)
+
+    if st.button("Find Largest"):
+        largest = FindLargest(num1, num2, num3)
+        st.success(f"The largest number is: {largest}")
+
+if _name_ == "main":
+    main()
